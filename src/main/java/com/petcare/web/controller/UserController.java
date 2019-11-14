997fcbae3cdbd1532d65779385cf134948035608
KBJ <KBJ@LAPTOP-505QE2KU>
2019-11-14 20:11:53 +0900

    Login 관련 일부 작업완료

---
+++ src/main/java/com/petcare/web/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.petcare.web.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.petcare.web.domains.LoginDTO;
+import com.petcare.web.domains.UserVO;
+import com.petcare.web.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public void loginGET(@ModelAttribute("dto") LoginDTO dto) {
+		
+	}
+	
+	@RequestMapping(value="/loginPost",method=RequestMethod.GET)
+	public void loginPOST(LoginDTO dto, HttpSession session, Model model) throws Exception{
+		UserVO vo=service.login(dto);
+		if(vo==null) {
+			return;
+		}
+		
+		model.addAttribute("userVO",vo);
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/petcare/web/mapper/UserMapperImpl.java
@@ -0,0 +1,25 @@
+package com.petcare.web.mapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.petcare.web.domains.LoginDTO;
+import com.petcare.web.domains.UserVO;
+
+@Repository
+public class UserMapperImpl implements UserMapper {
+
+	@Autowired
+	private SqlSession session;
+	
+	private static String namespace="com.petcare.web.mapper.UserMapper";
+
+	@Override
+	public UserVO login(LoginDTO dto) throws Exception {
+
+		
+		return session.selectOne(namespace+".login",dto);
+	}
+
+}
