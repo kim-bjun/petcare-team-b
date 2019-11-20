4a26e247d955d57fd098cfeb45d26b63d761f209
kim-bjun <41669067+kim-bjun@users.noreply.github.com>
2019-11-20 22:37:50 +0900

    Update UserService.java
    자동로그인 기능 추가

--- src/main/java/com/petcare/web/service/UserService.java
+++ src/main/java/com/petcare/web/service/UserService.java
@@ -1,9 +1,13 @@
 package com.petcare.web.service;
 
+import java.util.Date;
+
 import com.petcare.web.domains.LoginDTO;
 import com.petcare.web.domains.UserVO;
 
 public interface UserService {
 
 	public UserVO login(LoginDTO dto) throws Exception;
+	public void keepLogin(String userid,String sessionId, Date next) throws Exception;
+	public UserVO checkLoginBefore(String value);
 }
