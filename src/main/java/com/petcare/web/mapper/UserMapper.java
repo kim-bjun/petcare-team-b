81777d5a50198f37d3b1a43bef97f4af1216b5eb
kim-bjun <41669067+kim-bjun@users.noreply.github.com>
2019-12-01 14:58:02 +0900

    Update UserMapper.java
    LoginDTO 값 변경에 따라 속성값 변경

--- src/main/java/com/petcare/web/mapper/UserMapper.java
+++ src/main/java/com/petcare/web/mapper/UserMapper.java
@@ -12,7 +12,7 @@ public interface UserMapper {
 
 	public CustomerVO login(LoginDTO dto) throws Exception;
 	
-	public void keepLogin(@Param("userid") String userid,@Param("sessionId") String sessionId,@Param("next") Date next);
+	public void keepLogin(@Param("user_id") String userid,@Param("sessionId") String sessionId,@Param("next") Date next);
 	
 	public CustomerVO checkUserWithSessionKey(String value);
 }
