51f54c942e324abac1e563de468b5d8de6552ae1
kim-bjun <41669067+kim-bjun@users.noreply.github.com>
2019-12-01 23:08:40 +0900

    Update LoginDTO.java
    CustomerVO 카멜 적용에 따라 변경

--- src/main/java/com/petcare/web/domains/LoginDTO.java
+++ src/main/java/com/petcare/web/domains/LoginDTO.java
@@ -12,31 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginDTO {
 
-	private String user_id;
-	private String user_pass;
+	private String userId;
+	private String userPass;
 	private boolean useCookie;
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	public String getUser_pass() {
-		return user_pass;
-	}
-	public void setUser_pass(String user_pass) {
-		this.user_pass = user_pass;
-	}
-	public boolean isUseCookie() {
-		return useCookie;
-	}
-	public void setUseCookie(boolean useCookie) {
-		this.useCookie = useCookie;
-	}
-	@Override
-	public String toString() {
-		return "LoginDTO [user_id=" + user_id + ", user_pass=" + user_pass + ", useCookie=" + useCookie + "]";
-	}
+
 	
 
 
